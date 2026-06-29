2ff80d62d7593e9c8b4c9636052901b5886ead22
bannu-star-code <nitinchiliveri007@gmail.com>
2026-06-29 12:43:27 +0530

    Profile Page added

--- frontend/src/components/LeftSideBar.jsx
+++ frontend/src/components/LeftSideBar.jsx
@@ -4,6 +4,7 @@ import { FaInternetExplorer } from "react-icons/fa";
 import { LuLogOut } from "react-icons/lu";
 import { FaRegBookmark } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
+import { Link,useNavigate } from 'react-router-dom';
 
 
 const LeftSideBar = () => {
@@ -14,20 +15,20 @@ const LeftSideBar = () => {
           <img className='mx-3' src="https://img.magnific.com/free-vector/new-2023-twitter-logo-x-icon-design_1017-45418.jpg?semt=ais_hybrid&w=740&q=80" alt="twitter-logo" height={50} width={50} ></img>
         </div>
         <div className='my-4'>
-          <div className='flex items-center my-2 px-4 py-2 hover:bg-gray-200 hover:cursor-pointer rounded-full'>
+          <Link to="/" className='flex items-center my-2 px-4 py-2 hover:bg-gray-200 hover:cursor-pointer rounded-full'>
             <FaHome  size="28px"/>
             <h1 className='font-bold text-lg ml-2'>Home</h1>
-          </div>
+          </Link>
 
           <div className='flex items-center my-2 px-4 py-2 hover:bg-gray-200 hover:cursor-pointer rounded-full'>
             <FaInternetExplorer size="28px"/>
             <h1 className='font-bold text-lg ml-2'>Explore</h1>
           </div>
 
-          <div className='flex items-center my-2 px-4 py-2 hover:bg-gray-200 hover:cursor-pointer rounded-full'>
+          <Link to="/profile" className='flex items-center my-2 px-4 py-2 hover:bg-gray-200 hover:cursor-pointer rounded-full'>
             <CgProfile  size="28px"/>
             <h1 className='font-bold text-lg ml-2'>Profile</h1>
-          </div>
+          </Link>
 
 
           <div className='flex items-center my-2 px-4 py-2 hover:bg-gray-200 hover:cursor-pointer rounded-full'>
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { IoMdArrowBack } from "react-icons/io";
 
 const Profile = () => {
   return (
-    <div>Profile Name</div>
+    <div className='w-[50%]'>
+      <div>
+        <div className='flex items-center'>
+          <div className='p-2 rounded-full hover:bg-gray-200 hover:cursor-pointer'>
+            <IoMdArrowBack />
+          </div>
+          <div>
+          <h1>Nitin</h1>
+          <p>10 Posts</p>
+          </div>
+
+        </div>
+        <img src="https://pbs.twimg.com/profile_banners/1581707412922200067/1693248932/1080x360" alt="banner" ></img>
+      </div>
+    </div>
   )
 }
 
