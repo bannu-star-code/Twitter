5b65755347605801b15d200073d56f1f0a740694
bannu-star-code <nitinchiliveri007@gmail.com>
2026-06-30 10:01:55 +0530

    Profile Page Done

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -6,7 +6,7 @@ import Avatar from 'react-avatar';
 
 const Profile = () => {
   return (
-    <div className='w-[50%]'>
+    <div className='w-[50%] border-l border-r border-gray-300'>
       <div>
         <div className='flex items-center px-4 py-2'>
           <Link to="/" className='p-2 rounded-full hover:bg-gray-200 hover:cursor-pointer'>
@@ -19,8 +19,20 @@ const Profile = () => {
 
         </div>
         <img src="https://pbs.twimg.com/profile_banners/1581707412922200067/1693248932/1080x360" alt="banner" ></img>
-        <div className='absolute top-50 border-4 border-white rounded-full'>
+        <div className='absolute top-52 ml-2 border-4 border-white rounded-full'>
             <Avatar googleId="118096717852922241760" size="120" round={true} />
+        </div>
+        <div className='text-right m-4'>
+            <button className='px-4 py-1 hover:bg-gray-200 rounded-full border border-gray-400 '>Edit Profile</button>
+        </div>
+        <div className='mt-10'>
+            <h1 className='font-bold text-xl'>Nitin Chiliveri</h1>
+            <p>@NitinChiliveri</p>
+        </div>
+        <div className='m-4 text-sm'>
+            <p>
+                Exploring the web endless possibilities 
+            </p>
         </div>
       </div>
     </div>
